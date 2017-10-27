4a08760f137eb2f40239cb507ac41191347766de
Harsimran Singh <harsimransingh032@gmail.com>
2017-10-27 01:40:04 -0600

    all changes, remove extra folder; clean up

--- setup.py
+++ setup.py
@@ -1,22 +1,30 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 VERSION = '0.1-alpha'
 DOWNLOAD_URL = ('https://github.com/harsimrans/DropboxSyncer/archive/'
                 '{}.zip'.format(VERSION))
 REQUIRES = ['dropbox']
 
+PACKAGES = find_packages(exclude=["*.tests", "*.tests.*", "tests.*",
+                                    "tests"])
+
+add_keywords = dict(
+    entry_points={
+        'console_scripts': ['dsync=dropbox_sync.syncer:main'],
+    }, )
+
 setup(
     name='dropbox-sync',
     version=VERSION,
     license='MIT',
-    scripts=['dropbox-sync/syncer.py'],
     author='harsimran singh',
     author_email='harsimransingh032@gmail.com',
     download_url=DOWNLOAD_URL,
     install_requires=REQUIRES,
-    packages=['dropbox-sync'],
+    packages=PACKAGES,
     include_package_data=True,
     zip_safe=False,
     url='https://github.com/harsimrans/DropboxSyncer',
     description='Syncs dropbox folder mentioned realtime',
+     **add_keywords
 )
